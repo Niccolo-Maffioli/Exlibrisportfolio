c47afc1560003ea6d612dac0af3e8b5738b4828c
Niccolò Maffioli <76161234+Niccolo14946@users.noreply.github.com>
2022-04-11 17:39:58 +0200

    fix

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
-new kursor({
+// kursor
+/* new kursor({
     type: 1,
     removeDefaultCursor: true,
     color: '#C06F33',
-})
+}) */
 
-/* toggle menu */
+// toggle menu
 function myFunction() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
@@ -14,7 +15,7 @@ function myFunction() {
     }
 }
 
-/* accordion */
+// accordion
 var acc = document.getElementsByClassName("accordion");
 var i;
 
@@ -22,7 +23,7 @@ for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     /* Toggle between adding and removing the "active" class,
     to highlight the button that controls the panel */
-    this.classList.toggle("active");
+    this.classList.toggle("actives");
 
     /* Toggle between hiding and showing the active panel */
     var panel = this.nextElementSibling;
@@ -33,3 +34,5 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+// pagination
