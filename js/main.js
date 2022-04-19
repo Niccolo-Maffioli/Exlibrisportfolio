3e5d40fcb3411df74e32f7b0a63a0f16fdb9611f
Niccolò Maffioli <76161234+Niccolo14946@users.noreply.github.com>
2022-04-19 16:50:51 +0200

    fix accordions

--- js/main.js
+++ js/main.js
@@ -21,83 +21,18 @@ document.addEventListener("DOMContentLoaded", () => {
   })
 })
 
-/* var bar = document.getElementById("e-icon");
-var xicon = document.getElementById("x-icon");
-
-function toggle() {
-
-}
-
-addEventListener("click" function(){
-  
-} */
-
-
-
-
-// accordion
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("actives");
-
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
-
 // contact form
-$(document).ready(function(){
-  
-  //Hiding Labels Initially
-  $('form li').each(function(){
-    $(this).addClass('js-hide-label');
-  });
-  
-  //Now adding and removing classes on Events - keyup,blur,focus
-  $('form li').find('input,textarea').on('keyup blur focus',function(e) {
-    
-      var $this = $(this),
-          $parent = $this.parent();
-      
-      if(e.type=='keyup') {
-          if($this.val()==''){
-            $parent.addClass('js-hide-label');
-          }
-          else{
-            $parent.removeClass('js-hide-label') ;
-          }
-      }
-    else if(e.type=='blur'){
-      if($this.val()==''){
-        $parent.addClass('js-hide-label');
-      }
-      else{
-        $parent.removeClass('js-hide-label , js-highlight-label').addClass('js-unhighlight-label');
-      }
-    }
-    
-      else if(e.type=='focus'){
-        
-        if($this.val()==''){
-          $parent.addClass('js-hide-label');
-        }
-        else{
-        $parent.addClass('js-highlight-label');
-        }
-        
-      }
-    
-  });
-  
-});
 
+var target_popup = function(form) {
+  window.open('',//URL should be blank so that it will take form attributes.
+              'UniqueWindowName', //window name
+              'width=400,height=400,resizeable,scrollbars');
+  form.target = 'UniqueWindowName';
+}
+
+function myFunction( win ) {
+  // do stuff with "win", a newly-opened window
+	alert('Sent Successfully ✅');
+	console.log(win);
+	document.getElementById("form").submit();
+}
