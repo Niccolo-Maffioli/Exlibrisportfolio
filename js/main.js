485497f88afe02c51065255132e657831ae0d80c
Niccolò Maffioli <76161234+Niccolo14946@users.noreply.github.com>
2022-04-24 18:33:31 +0200

    add and fix loader

--- js/main.js
+++ js/main.js
@@ -87,3 +87,24 @@ function Easter(keyCombination, onComplete , debug)
         }
     }
 }
+
+// loader
+
+window.onload = loader;
+  function loader() {
+    let loaderPage = document.querySelector('.loader-page');
+    let counter = document.querySelector('.loader-counter');
+    let fill = document.querySelector('.loader-fill');
+    let amount = 10;
+    let interval = setInterval(loop, 50);
+    function loop() {
+      if (amount >= 100) {
+        clearInterval(interval);
+        loaderPage.style.visibility = 'hidden';
+        loaderPage.style.opacity = '0'
+      } else {
+        amount++;
+        fill.style.width = amount + '%';
+    }
+  }
+}
