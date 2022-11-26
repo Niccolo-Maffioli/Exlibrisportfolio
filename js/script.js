8ce426d766f8509dca01fe8e3b093a12f22aac35
Niccolò Maffioli <76161234+Niccolo14946@users.noreply.github.com>
2022-11-26 04:32:28 +0100

    Update

--- js/script.js
+++ js/script.js
@@ -98,3 +98,22 @@ function asideSectionTogglerBtn() {
         allSection[i].classList.toggle("open");
     }
 }
+
+// ANCHOR Loader
+window.onload = loader;
+        function loader() {
+          let loaderPage = document.querySelector('.loader-page');
+          /* let fill = document.querySelector('.loader-fill'); */
+          let amount = 10;
+          let interval = setInterval(loop, 50);
+          function loop() {
+            if (amount >= 100) {
+              clearInterval(interval);
+              loaderPage.style.visibility = 'hidden';
+              loaderPage.style.opacity = '0';
+            } else {
+              amount++;
+              /* fill.style.width = amount + '%'; */
+          }
+        }
+      }
